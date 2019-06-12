13dbfb46588bc330280a27dff3f2d9fac5ff1263
jameswel <jameswelsh96@icloud.com>
2019-06-12 12:14:36 +0200

    HammerJS and routing

--- hshlholzshop/src/app/app-routing.module.ts
+++ hshlholzshop/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+// Components
 import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: 'user/:id', component: UserComponent },
   { path: 'dashboard', component: DashboardComponent }
 ];
 
