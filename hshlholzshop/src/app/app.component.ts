9563c480c6d867c5f1a267a0a61f6570c9b105c3
jameswel <jameswelsh96@icloud.com>
2019-06-27 20:30:42 +0200

    Design Update

--- hshlholzshop/src/app/app.component.ts
+++ hshlholzshop/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loginregistrationservice } from './models/Loginregistrationservice';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoggedIn = true;
+  constructor(private service: Loginregistrationservice) { }
+  ngOnInit(): void {
+    this.isLoggedIn = this.service.isLoggedIn()
+    console.log(this.isLoggedIn)
+  }
+  
 }
